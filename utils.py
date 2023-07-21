e918c5d269364c5201cc85b9865fa6a54cfd2a70
KaziArman <kazi.arman.mist@gmail.com>
2023-07-21 21:53:25 +0600

    Checking Multiple Pushing

--- utils.py
+++ utils.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import streamlit as st
 
 def local_css(file_name):
@@ -6,13 +5,4 @@ def local_css(file_name):
         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 def remote_css(url):
-=======
-import streamlit as st
-
-def local_css(file_name):
-    with open(file_name) as f:
-        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
-
-def remote_css(url):
->>>>>>> 52de17b5fd100f8a16c3380f94ce8b9799d13233
     st.markdown(f'<link href="{url}" rel="stylesheet">', unsafe_allow_html=True)
