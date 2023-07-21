befa5ee23612027d72293482a99563641b876c1e
Kazi Arman Ahmed <kazi.arman.mist@gmail.com>
2023-07-21 22:10:37 +0600

    Update youtube_transcript.py

--- youtube_transcript.py
+++ youtube_transcript.py
@@ -19,27 +19,3 @@ class YT_transcript:
         documents = loader.load_data(ytlinks=[self.lnk])
         text=documents[0].text
         return text
-class YT_transcript:
-    def __init__(self,lnk):
-        self.lnk = lnk
-
-    def script(self):
-        from llama_index import download_loader
-        import os
-        from transformers import GPT2TokenizerFast
-        from langchain.document_loaders import PyPDFLoader
-        from langchain.text_splitter import RecursiveCharacterTextSplitter
-        from langchain.embeddings import OpenAIEmbeddings
-        from langchain.vectorstores import FAISS
-        from langchain.chains.question_answering import load_qa_chain
-        from langchain.llms import OpenAI
-        from langchain.chains import ConversationalRetrievalChain
-        # Step 2: Save to .txt and reopen (helps prevent issues)
-        #with open('output.txt', 'r') as f:
-        #    text = f.read()
-        YoutubeTranscriptReader = download_loader("YoutubeTranscriptReader")
-        loader = YoutubeTranscriptReader()
-        documents = loader.load_data(ytlinks=[self.lnk])
-        text=documents[0].text
-        return text
-
