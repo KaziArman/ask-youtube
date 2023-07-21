fad56abf8cabb0cce31d998c1f48c8df125d2746
KaziArman <kazi.arman.mist@gmail.com>
2023-07-21 21:10:50 +0600

    Pushing to github

---
+++ youtube_transcript.py
@@ -0,0 +1,21 @@
+class YT_transcript:
+    def __init__(self,lnk):
+        self.lnk = lnk
+
+    def script(self):
+        from llama_index import download_loader
+        import os
+        from transformers import GPT2TokenizerFast
+        from langchain.document_loaders import PyPDFLoader
+        from langchain.text_splitter import RecursiveCharacterTextSplitter
+        from langchain.embeddings import OpenAIEmbeddings
+        from langchain.vectorstores import FAISS
+        from langchain.chains.question_answering import load_qa_chain
+        from langchain.llms import OpenAI
+        from langchain.chains import ConversationalRetrievalChain
+
+        YoutubeTranscriptReader = download_loader("YoutubeTranscriptReader")
+        loader = YoutubeTranscriptReader()
+        documents = loader.load_data(ytlinks=[self.lnk])
+        text=documents[0].text
+        return text
